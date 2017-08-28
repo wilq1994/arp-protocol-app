147bf01b888bbeebb6f9f12ca64255cff57c6f4c
Bartosz Wilk <bartek94@gmail.com>
2017-08-28 23:59:37 +0200

    recursive algorithm - first approach

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <div class="list-computers">
+      <computer v-for="(computer, key) in computers" :key="key" :id="key" :value="computer.value"></computer>
+    </div>
+    <button v-on:click="runSearch(1, 'b')">Start</button>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+  import Computer from './components/Computer'
 
-export default {
-  name: 'app',
-  components: {
-    Hello
+  export default {
+    name: 'app',
+    components: {
+      Computer
+    },
+    data: () => {
+      return {
+        computers: {
+          1: {
+            value: 'a',
+            neighbours: [2]
+          },
+          2: {
+            value: 'b',
+            neighbours: [1, 3]
+          },
+          3: {
+            value: 'c',
+            neighbours: [2,4]
+          },
+          4: {
+            value: 'd',
+            neighbours: [3]
+          }
+        }
+      }
+    },
+    methods: {
+      runSearch(target, value){
+        let _p = [1]
+        console.log(this.search('d', 2, _p, [1]))
+      },
+      search(value, id, path, visited = []){
+        if(visited.includes(id)) return false
+
+        const that = this;
+        const _path = [id];
+        visited.push(id)
+
+        if(this.computers[id].value === value){
+          return true
+        }else{
+          const sorted = this.computers[id]
+                              .neighbours
+                              .filter((item) => {
+                                if(!visited.includes(item)) return true
+                                return false
+                              })
+
+          for(let key in sorted){
+            if(this.search(value, sorted[key], _path, visited)){
+              _path.push(sorted[key])
+              console.log(path.concat(_path));
+              return true
+            }
+          }
+        }
+
+        return false
+      }
+    }
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    max-width: 800px;
+    margin: 60px auto 0;
+  }
+
+  .list-computers {
+    display: flex;
+    justify-content: space-between;
+  }
 </style>
---
+++ src/components/Computer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="computer">
+    <div class="computer__status"></div>
+    <div>Computer {{ id }}</div>
+    <div>Value {{ value }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'computer',
+    props: ['id', 'value'],
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .computer__status {
+    background: #000;
+    width: 20px;
+    height: 20px;
+    border-radius: 20px;
+  }
+</style>
